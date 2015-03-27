6c9e7534a6a557d9b5685a230a03f597746ffd44
kylegmaxwell <kyle.g.maxwell@gmail.com>
2015-03-27 09:28:14 -0700

    webgl
    cube render working with index buffer

--- LearnWebGL/shaders/frag.glsl
+++ LearnWebGL/shaders/frag.glsl
@@ -14,7 +14,11 @@ void main(void)
     
     //gl_FragColor = vColor;
     
-    gl_FragColor = vNormal;
+    vec4 col = vNormal;
+    col += vec4(1,1,1,1);
+    col *= vec4(0.5,0.5,0.5,0.5);
+    
+    gl_FragColor = col;
     
     float d = vDepth*0.05+0.1;
     //gl_FragColor = vec4(d,d,d,1);
